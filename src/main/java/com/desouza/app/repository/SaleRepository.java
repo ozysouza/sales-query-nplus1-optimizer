3ce740b563e088890bace5fee7c8499bb94c21a5
ozysouza <ozysouzaa@gmail.com>
2025-06-18 00:09:36 -0400

    Add JPA Query to return summary based on DTO

--- src/main/java/com/desouza/app/repository/SaleRepository.java
+++ src/main/java/com/desouza/app/repository/SaleRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import com.desouza.app.dto.SalesSummaryDTO;
 import com.desouza.app.entities.Sale;
 
 public interface SaleRepository extends JpaRepository<Sale, Long> {
@@ -22,5 +23,18 @@ public interface SaleRepository extends JpaRepository<Sale, Long> {
             AND UPPER(obj.seller.name) LIKE UPPER(CONCAT('%', :name, '%'))
             """)
     Page<Sale> getReport(LocalDate minDate, LocalDate maxDate, Pageable pageable, String name);
+
+    @Query(value = """
+            SELECT new com.desouza.app.dto.SalesSummaryDTO(
+                obj.seller.name,
+                SUM(obj.amount)
+            )
+            FROM Sale obj
+            INNER JOIN obj.seller
+            WHERE obj.date BETWEEN :minDate AND :maxDate
+            AND UPPER(obj.seller.name) LIKE UPPER(CONCAT('%', :name, '%'))
+            GROUP BY obj.seller.name
+            """)
+    Page<SalesSummaryDTO> getSummary(LocalDate minDate, LocalDate maxDate, Pageable pageable, String name);
 
 }
