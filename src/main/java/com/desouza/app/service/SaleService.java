9f8cbe869bc6f31f97f178ef4abdc85904028f26
ozysouza <ozysouzaa@gmail.com>
2025-06-17 23:46:55 -0400

    Handle NoSuchElementException in case sale ID is not valid

--- src/main/java/com/desouza/app/service/SaleService.java
+++ src/main/java/com/desouza/app/service/SaleService.java
@@ -1,5 +1,6 @@
 package com.desouza.app.service;
 
+import java.util.NoSuchElementException;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.desouza.app.dto.SaleMinDTO;
 import com.desouza.app.entities.Sale;
 import com.desouza.app.repository.SaleRepository;
+import com.desouza.app.service.exceptions.ResourceNotFoundException;
 
 @Service
 public class SaleService {
@@ -18,9 +20,14 @@ public class SaleService {
 
     @Transactional(readOnly = true)
     public SaleMinDTO findById(Long id) {
-        Optional<Sale> result = repository.findById(id);
-        Sale entity = result.get();
-        return new SaleMinDTO(entity);
+        try {
+            Optional<Sale> result = repository.findById(id);
+            Sale entity = result.get();
+
+            return new SaleMinDTO(entity);
+        } catch (NoSuchElementException e) {
+            throw new ResourceNotFoundException("Sale with ID " + id + " was not found");
+        }
     }
 
 }
