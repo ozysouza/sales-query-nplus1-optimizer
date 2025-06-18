78563a1bf02c56de1cbad1247f38f86e962f1245
ozysouza <ozysouzaa@gmail.com>
2025-06-17 23:58:02 -0400

    Add JPA Query to return report, this query has N+1 issue

--- src/main/java/com/desouza/app/repository/SaleRepository.java
+++ src/main/java/com/desouza/app/repository/SaleRepository.java
@@ -1,9 +1,22 @@
 package com.desouza.app.repository;
 
+import java.time.LocalDate;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.desouza.app.entities.Sale;
 
-public interface SaleRepository extends JpaRepository<Sale, Long>{
+public interface SaleRepository extends JpaRepository<Sale, Long> {
+
+    @Query(value = """
+            SELECT obj FROM Sale obj
+            JOIN obj.seller
+            WHERE obj.date BETWEEN :minDate AND :maxDate
+            AND UPPER(obj.seller.name) LIKE UPPER(CONCAT('%', :name, '%'))
+            """)
+    Page<Sale> getReport(LocalDate minDate, LocalDate maxDate, Pageable pageable, String name);
 
 }
