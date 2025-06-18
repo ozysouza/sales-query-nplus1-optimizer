35ee0dad07afd70e90fac817e9b5fdb8761916b0
ozysouza <ozysouzaa@gmail.com>
2025-06-17 23:59:21 -0400

    Create getMapping to return report, pageable and request parameters

--- src/main/java/com/desouza/app/controllers/SaleController.java
+++ src/main/java/com/desouza/app/controllers/SaleController.java
@@ -1,13 +1,17 @@
 package com.desouza.app.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.desouza.app.dto.SaleMinDTO;
+import com.desouza.app.dto.SalesReportDTO;
 import com.desouza.app.service.SaleService;
 
 @RestController
@@ -21,6 +25,16 @@ public class SaleController {
     public ResponseEntity<SaleMinDTO> findById(@PathVariable Long id) {
         SaleMinDTO dto = service.findById(id);
         return ResponseEntity.ok(dto);
+    }
+
+    @GetMapping(value = "/report")
+    public ResponseEntity<Page<SalesReportDTO>> getReport(Pageable pageable,
+            @RequestParam(defaultValue = "") String minDate,
+            @RequestParam(defaultValue = "") String maxDate,
+            @RequestParam(defaultValue = "") String name) {
+
+        Page<SalesReportDTO> report = service.getReport(pageable, minDate, maxDate, name);
+        return ResponseEntity.ok(report);
     }
 
 }
