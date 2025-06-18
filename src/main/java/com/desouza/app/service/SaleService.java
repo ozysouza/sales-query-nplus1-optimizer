44090f1dea952073553b6689a16c670fb4002cbc
ozysouza <ozysouzaa@gmail.com>
2025-06-17 23:26:07 -0400

    Create SaleService to quickly return a sale by ID usign JPA Hibernate

---
+++ src/main/java/com/desouza/app/service/SaleService.java
@@ -0,0 +1,26 @@
+package com.desouza.app.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.desouza.app.dto.SaleMinDTO;
+import com.desouza.app.entities.Sale;
+import com.desouza.app.repository.SaleRepository;
+
+@Service
+public class SaleService {
+
+    @Autowired
+    private SaleRepository repository;
+
+    @Transactional(readOnly = true)
+    public SaleMinDTO findById(Long id) {
+        Optional<Sale> result = repository.findById(id);
+        Sale entity = result.get();
+        return new SaleMinDTO(entity);
+    }
+
+}
