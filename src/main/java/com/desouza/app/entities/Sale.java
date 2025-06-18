bba31dc81a07e05dc6061693ce492cc0ee4fd1e9
ozysouza <ozysouzaa@gmail.com>
2025-06-17 22:18:44 -0400

    Create Sale entity

---
+++ src/main/java/com/desouza/app/entities/Sale.java
@@ -0,0 +1,70 @@
+package com.desouza.app.entities;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_sale")
+public class Sale {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Integer visited;
+    private Integer deals;
+    private Double amount;
+    private LocalDate date;
+
+    public Sale() {
+    }
+
+    public Sale(Long id, Integer visited, Integer deals, Double amount, LocalDate date) {
+        this.id = id;
+        this.visited = visited;
+        this.deals = deals;
+        this.amount = amount;
+        this.date = date;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    public Integer getVisited() {
+        return visited;
+    }
+
+    public void setVisited(Integer visited) {
+        this.visited = visited;
+    }
+
+    public Integer getDeals() {
+        return deals;
+    }
+
+    public void setDeals(Integer deals) {
+        this.deals = deals;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+}
